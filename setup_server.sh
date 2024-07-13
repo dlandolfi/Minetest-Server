61fb8fc837cf773d2caabcf14e498f9aa0a07d85
Daniel Landolfi <16345531+dlandolfi@users.noreply.github.com>
2024-07-13 13:27:25 -0400

    Create setup_server.sh

---
+++ setup_server.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Add Minetest PPA repository
+add-apt-repository ppa:minetestdevs/stable -y
+
+# Update package lists
+apt update
+
+# Upgrade existing packages
+apt upgrade -y
+
+# Install Minetest server
+apt install minetest-server -y
+
+# Configure UFW firewall
+ufw default deny incoming
+ufw default allow outgoing
+ufw allow OpenSSH
+ufw allow 30000/udp
+ufw enable
+
+# Disable Minetest server service
+systemctl disable minetest-server.service
+
+# Create and enable swap file
+dd if=/dev/zero of=/swapfile bs=1M count=1024
+chmod 600 /swapfile
+mkswap /swapfile
+swapon /swapfile
+echo '/swapfile none swap sw 0 0' | tee -a /etc/fstab
+
+# Reboot the server
+reboot
