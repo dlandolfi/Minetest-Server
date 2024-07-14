ef73286ab61453f778a788c3f2c94d93df75bec0
Daniel Landolfi <16345531+dlandolfi@users.noreply.github.com>
2024-07-14 14:35:45 -0400

    updating VL setup

--- setup_voxelibre.sh
+++ setup_voxelibre.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Navigate to the .minetest directory in the home directory
-cd ~/.minetest
+cd /var/games/minetest-server/.minetest
 
 # Create a directory named "games" if it doesn't already exist
 mkdir -p games
@@ -11,5 +11,3 @@ cd games
 
 # Clone the repository from the given URL
 git clone https://git.minetest.land/VoxeLibre/VoxeLibre.git
-
-echo "Repository cloned successfully!"
