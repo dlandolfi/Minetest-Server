a58eb6dc7b17fb7cc71baff889b311c0477f14f0
Daniel Landolfi <16345531+dlandolfi@users.noreply.github.com>
2024-07-14 14:44:18 -0400

    extending setting up VL

--- setup_voxelibre.sh
+++ setup_voxelibre.sh
@@ -11,3 +11,9 @@ cd games
 
 # Clone the repository from the given URL
 git clone https://git.minetest.land/VoxeLibre/VoxeLibre.git
+
+# Copy the minetest.conf from ~/Minetest-Server/ to /etc/minetest/
+cp ~/Minetest-Server/minetest.conf /etc/minetest/minetest.conf
+
+# Restart the minetest service
+systemctl restart minetest-server.service
